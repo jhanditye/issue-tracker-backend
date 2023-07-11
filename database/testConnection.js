0e3fbada610432c81489f16c619ffc2d2ab81c9c
jhanditye <jamiehanditye@gmail.com>
2023-07-11 17:38:45 +0100

    set up basic fastpi app with desired folder structuring

--- database/testConnection.js
+++
@@ -1,5 +0,0 @@
-const db = require('./database');
-
-db.any('SELECT 1')
-  .then(() => console.log('Database connection successful'))
-  .catch(err => console.error('Database connection error', err));
