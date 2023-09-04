1998341e4c7d567c46afef03f91d86a6587c9bab
jhanditye <jamiehanditye@gmail.com>
2023-09-04 19:00:01 +0100

    Add Teams table and User-team relation

--- app/models.py
+++ app/models.py
@@ -1,10 +1,16 @@
 from .database import Base
-
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Table
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from sqlalchemy.sql.expression import text
 
+# Many-to-many association table for User and Teams
+user_team_association_table = Table(
+    "user_teams",
+    Base.metadata,
+    Column("user_id", Integer, ForeignKey("users.id"), primary_key=True),
+    Column("team_id", Integer, ForeignKey("teams.id"), primary_key=True),
+)
 
 class User(Base):
     __tablename__ = "users"
@@ -13,8 +19,8 @@ class User(Base):
     email = Column(String, unique=True, index=True, nullable=False)
     password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
-    issues = relationship("Issue", back_populates="owner")
-
+    issues = relationship("Issue", back_populates="assigned_user")
+    teams = relationship("Team", secondary=user_team_association_table, back_populates="users")
 
 class Issue(Base):
     __tablename__ = "issues"
@@ -23,6 +29,16 @@ class Issue(Base):
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+    assigned_user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    assigned_user = relationship("User", back_populates="issues")
+    team_id = Column(Integer, ForeignKey("teams.id", ondelete="CASCADE"), nullable=False)
+    team = relationship("Team", back_populates="issues")
 
-    owner_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
-    owner = relationship("User", back_populates="issues")
+class Team(Base):
+    __tablename__ = "teams"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String, nullable=False)
+    description = Column(String, nullable=False)
+    users = relationship("User", secondary=user_team_association_table, back_populates="teams")
+    issues = relationship("Issue", back_populates="team")
